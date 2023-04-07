77ca6a7ab9b7ba7aa079c948ac440327de119773
Umarov Yuriy <kumar13-05@mail.ru>
2023-04-07 19:35:02 +0530

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         int check = 100;
-        int mUp = 900;
+        int mUp = 1010;
         int bonus = mUp / 100;
 
-        if (mUp >= 1100) {
+        if (mUp >= 1000) {
             bonus = mUp + bonus + check;
         } else {
             bonus = 0;
